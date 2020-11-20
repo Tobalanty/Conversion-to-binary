864807ff13fe5254587214e0bedf04f5e10e84ac
Tobalanty <gg1gg141@gmail.com>
2020-11-20 21:23:45 +0300

    Initial commit

---
+++ Conversion-to-binary.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+int inputInteger() {                                                                    //Ввод
+
+    std::cout << "Input an integer (0 -- 255): ";                                     
+    int a;
+    std::cin >> a;
+
+    return a;
+}
+
+bool conversion(int a) {                                                                //Возвращаю 1 или 0 в зависимости от результата деления
+
+    return !(a % 2 == 0);
+}
+
+int main()
+{
+
+    int a = inputInteger();
+    bool binary[8];
+
+    std::cout << "\n";
+
+    for (int i = 0; i < 8 ; ++i) {                                                     //Записываю в массив значение каждого бита
+        binary[i] = conversion(a);
+        a = a / 2;
+    }
+
+
+    for (int i = 1; i < 9; ++i) {                                                       //Вывожу массив с конца и ставлю пробел после каждого 4-го бита
+        std::cout << binary[7-(i-1)];
+        if (i % 4 == 0)  {
+            std::cout << " ";
+        }
+    }
+
+    system("pause");
+    return 0;
+}
