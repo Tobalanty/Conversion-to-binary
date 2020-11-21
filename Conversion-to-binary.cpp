ed0a187886d89aa84720a41513c04222433f99b3
Tobalanty <gg1gg141@gmail.com>
2020-11-21 17:46:21 +0300

    Теперь и для отрицательных чисел

--- Conversion-to-binary.cpp
+++ Conversion-to-binary.cpp
@@ -1,39 +1,90 @@
 ﻿#include <iostream>
 
+const int n{ 8 };
+
 int inputInteger() {                                                                    //Ввод
 
-    std::cout << "Input an integer (0 -- 255): ";                                     
+    std::cout << "Input a "<< n << "-bit integer : ";                                     
     int a;
     std::cin >> a;
 
     return a;
 }
 
-bool conversion(int a) {                                                                //Возвращаю 1 или 0 в зависимости от результата деления
+bool conversionForPositive(int a) {                                                      //Возвращаю 1 или 0 в зависимости от результата выполнения функции
 
     return !(a % 2 == 0);
+}
+
+
+void output(int a) {
+
+    bool binary[n];
+
+    if (a > 0) {
+        for (int i = 0; i < n; ++i) {                                                     //Записываю в массив значение каждого бита
+            binary[i] = conversionForPositive(a);
+            a /= 2;
+        }
+
+        for (int i = 1; i < n + 1; ++i) {                                                 //Вывожу массив с конца и ставлю пробел после каждого 4-го бита
+            std::cout << binary[n - i];
+            if (i % 4 == 0) {
+                std::cout << " ";
+            }
+        }
+    }
+    else
+        if (a == 0) {                                                                     //Если введён 0, то вывожу бинарный 0
+            for (int i = 1; i < n + 1; ++i) {
+                std::cout << "0";
+                if (i % 4 == 0) {
+                    std::cout << " ";
+                }
+            }
+        }
+        else
+            if (a < 0) {                                                                  //Если число отрицательное, то использую метод "two's complement"
+                a *= (-1);
+                for (int i = 0; i < n; ++i) {                                             //Записываю в массив значение каждого бита (инвертированно) 
+                    binary[i] = !(conversionForPositive(a));
+                    a /= 2;                                                               //Делю на 2 для следующей итерации
+                }
+
+                if (binary[0] == false) {                                                 //Добавляю единицу к получившемуся двочиному числу
+                    binary[0] = true;
+                }
+                else {
+                        for (int i = 0; i < n; ++i) {
+
+                            if (binary[i] == true) {
+                            binary[i] = false;
+                            }
+                            else {
+                                binary[i] = true;
+                                break;
+                            }
+
+                        }
+                }
+
+                for (int i = 1; i < n + 1; ++i) {                                          //Вывожу полученное двочиное число и ставлю пробел после каждого 4-го бита
+                    std::cout << binary[n - i];
+                    if (i % 4 == 0) {
+                        std::cout << " ";
+                    }
+                }
+            }
 }
 
 int main()
 {
 
     int a = inputInteger();
-    bool binary[8];
 
     std::cout << "\n";
 
-    for (int i = 0; i < 8 ; ++i) {                                                     //Записываю в массив значение каждого бита
-        binary[i] = conversion(a);
-        a = a / 2;
-    }
-
-
-    for (int i = 1; i < 9; ++i) {                                                       //Вывожу массив с конца и ставлю пробел после каждого 4-го бита
-        std::cout << binary[7-(i-1)];
-        if (i % 4 == 0)  {
-            std::cout << " ";
-        }
-    }
+    output(a);
 
     system("pause");
     return 0;
